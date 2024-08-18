25e7aef18dfcdb60409fd75962207e268b015711
wangzhen <wangzhenaaa7@gmail.com>
2024-08-18 15:34:53 +0800

    更新README.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # shell_tools
 #TODO: Need one app to receive the success message from the Linux/Mac/Win \
   asynchronous job.
+HUAWEI EcoDev doesn't support ubuntu directly, it only supports Mac/Win.
--- git_tools/git_clone.sh
+++ git_tools/git_clone.sh
@@ -4,7 +4,7 @@ git_repository=$1
 echo "$git_repository"
 ret=$(git clone $1 2>&1)
 echo "ret: $ret"
-while [[ $ret =~ '访问权限'|'过早' ]];
+while [[ $ret =~ '访问权限'|'过早'|'flush 包' ]];
 do
   ret=$(git clone $1 2>&1)
 done
